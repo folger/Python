33c8cf1ca7735c4cd7b04b5997bf722b915aff30
folger <folger@originlab.com.cn>
2014-01-10 18:57:11 +0800

    replace network images path with local path

--- SCVHtml/GenerateLTFuncHTML.py
+++ SCVHtml/GenerateLTFuncHTML.py
@@ -1,10 +1,10 @@
 funcs_descriptions = {}
-with open(r'C:\Dropbox\LTFuncs.txt', 'r') as f:
+with open(r'LTFuncs.txt', 'r') as f:
   for line in f:
     entries = line.split('\t'*10)
     funcs_descriptions[entries[0].split('(')[0].lower()] = (entries[0], entries[1] if len(entries) > 1 else "")
 
-with open(r'C:\Dropbox\SCVFuncs.txt', 'r') as f:
+with open(r'SCVFuncs.txt', 'r') as f:
   s = ''
 
   for line in f:
@@ -15,10 +15,10 @@ with open(r'C:\Dropbox\SCVFuncs.txt', 'r') as f:
     if line.endswith('$') or line.endswith(')'): # function
       try:
         funcs_description = funcs_descriptions[line.split('(')[0].lower()]
+        s += '<tr><td><a href="/junk">%s</a></td><td>%s</td></tr>' % (funcs_description[0], funcs_description[1].strip())
       except KeyError as e:
         pass
         # print("Missing function: %s" % line)
-      s += '<tr><td><a href="/junk">%s</a></td><td>%s</td></tr>' % (funcs_description[0], funcs_description[1].strip())
     else: # category
       if len(s):
         s += '</table>'
@@ -26,5 +26,6 @@ with open(r'C:\Dropbox\SCVFuncs.txt', 'r') as f:
 
   if len(s):
     s += '</table>'
+  s = s.replace('/images/ltwiki/math/', './images/')
   print(s)
 
