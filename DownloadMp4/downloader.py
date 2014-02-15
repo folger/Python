3ebecc24ed0ac0cb627ab5159de7c430d6ae1ee9
folger <folger6@gmail.com>
2014-02-15 17:23:41 +0800

    update downloader for mac

--- DownloadMp4/downloader.py
+++ DownloadMp4/downloader.py
@@ -1,5 +1,8 @@
 from urllib.request import urlretrieve
 
+# filefmt = r"G:\OnePiece\海贼王第%s集.%03d.mp4"
+filefmt = "/Users/lunbest/OnePiece/生活大爆炸第%s集.%03d.mp4"
+
 with open('Now.txt') as f:
     album = ''
     part = 0
@@ -12,8 +15,7 @@ with open('Now.txt') as f:
             album = line
             part = 1
         else:
-            # filename = r"G:\OnePiece\海贼王第%s集.%03d.mp4" % (album, part)
-            filename = r"/Users/lunbest/OnePiece/生活大爆炸第%s集.%03d.mp4" % (album, part)
+            filename = filefmt % (album, part)
             urlretrieve(line, filename)
             print("%s.%03d" %(album, part))
 
