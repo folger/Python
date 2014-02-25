c5befc15932a504f3abc1e71eff30b78b4f5a68a
folger <folger@originlab.com.cn>
2014-02-25 11:05:59 +0800

    parsing labtalk functions html remove LF in description

--- SCVHtml/ParseLTFunctionsHTML.py
+++ SCVHtml/ParseLTFunctionsHTML.py
@@ -3,18 +3,18 @@ from urllib.request import urlopen
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
-      super(MyHTMLParser, self).__init__()
-      self.table = False
+        super(MyHTMLParser, self).__init__()
+        self.table = False
 
-      self.col = 0
-      self.a = False
+        self.col = 0
+        self.a = False
 
-      self.results = []
+        self.results = []
 
     def handle_starttag(self, tag, attrs):
         if tag == "table":
-          if attrs and attrs[0][0] == 'class' and attrs[0][1] == 'simple LTFunc':
-            self.table = True
+            if attrs and attrs[0][0] == 'class' and attrs[0][1] == 'simple LTFunc':
+                self.table = True
 
         if self.table:
           if tag == "tr":
@@ -27,43 +27,43 @@ class MyHTMLParser(HTMLParser):
               self.a = True
           else:
             if self.isFunction() or self.isDescription():
-              self.description += "<%s %s>" % (tag, ' '.join(['%s="%s"' % (key, value) for (key, value) in attrs]))
+              self.description += "<%s %s>" % (tag, ' '.join(['%s="%s"' % (key, value.replace("\n", "")) for (key, value) in attrs]))
         
     def handle_endtag(self, tag):
         if self.table:
           if tag == "table":
             self.table = False
 
-          if self.table:
+        if self.table:
             if tag == "td":
-              if self.isDescription():
-                # print(self.description.strip())
-                self.results.append(self.description.strip())
+                if self.isDescription():
+                    # print(self.description.strip())
+                    self.results.append(self.description.strip())
             elif tag == "a":
-              if self.isFunction():
-                self.a = False
-                self.description += "\t" * 10
+                if self.isFunction():
+                    self.a = False
+                    self.description += "\t" * 10
             else:
-              if self.isFunction() or self.isDescription():
-                self.description += "</%s>" % tag
+                if self.isFunction() or self.isDescription():
+                    self.description += "</%s>" % tag
           
     def handle_data(self, data):
         if self.isFunction() or self.isDescription():
-          self.description += data
+            self.description += data.replace("\n", "")
 
     def isFunction(self):
-      return self.col == 1 and self.a
+        return self.col == 1 and self.a
 
     def isDescription(self):
-      return self.col == 2
+        return self.col == 2
 
         
 #with open(r'd:\aaa\Script\LabTalk-Supported_Functions.html') as f:
 with urlopen("http://wikis/docwiki/index.php?title=UserGuide%3ALabTalk-Supported_Functions") as r:
-  # print(f.read().decode())
-  parser = MyHTMLParser()
-  parser.feed(r.read().decode())
-  with open(r'g:\CheckCode\Python\SCVHtml\LTFuncs.txt', 'w', encoding='utf-8-sig') as f:
-    f.write('\n'.join(parser.results))
+    # print(f.read().decode())
+    parser = MyHTMLParser()
+    parser.feed(r.read().decode())
+    with open(r'g:\CheckCode\Python\SCVHtml\LTFuncs.txt', 'w', encoding='utf-8-sig') as f:
+        f.write('\n'.join(parser.results))
 
 
