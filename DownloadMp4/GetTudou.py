32ae0596269fa19ae4fd3f5425367b5201e2b5be
folger <folger@originlab.com.cn>
2014-04-04 12:59:06 +0800

    update GetTudou.py regex

--- DownloadMp4/GetTudou.py
+++ DownloadMp4/GetTudou.py
@@ -1,7 +1,7 @@
 import re
 from GetAddress import OutputAddrs
 
-preSrc = re.compile("(http://www.tudouxia.com/albumplay/\w+/\w+.html) (\d+)")
+preSrc = re.compile("(http://www.tudouxia.com/albumplay/\w+/(?:-|\w)+.html) (\d+)")
 preDes = re.compile('http://f.youku.com/player/getFlvPath/sid/\w+/st/mp4/fileid/[^"]+')
 
 def polishAddress(all):
