c521906dbe65c284e512f932cac401f68afbae8d
folger <folger@originlab.com.cn>
2014-04-07 12:14:51 +0800

    update GetSohu.py to latest web content source

--- DownloadMp4/GetSohu.py
+++ DownloadMp4/GetSohu.py
@@ -2,7 +2,7 @@ import re
 from GetAddress import OutputAddrs
 
 preSrc = re.compile("(http://tv.sohuxia.com/\d+/n\d+\.shtml) (\d+)")
-preDes = re.compile('http://\d+\.\d+\.\d+\.\d+[^"]+')
+preDes = re.compile('http://(?:\d+\.\d+\.\d+\.\d+|sohu\.soooner\.com)[^"]+')
 
 def polishAddress(all):
     block = 5
