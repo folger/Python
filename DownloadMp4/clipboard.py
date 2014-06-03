9adbf6428689f45a3599a63fc4588d5904285b8e
folger <folger@originlab.com.cn>
2014-06-03 20:27:41 +0800

    polish clipboard.py

--- DownloadMp4/clipboard.py
+++ DownloadMp4/clipboard.py
@@ -1,19 +1,24 @@
-import re
-
 from tkinter import Tk
 r = Tk()
 #r.clipboard_clear()
 #r.clipboard_append('I am here')
 result = r.clipboard_get()
-r.destroy()
-
-preDes = re.compile('http://(?:\d+\.\d+\.\d+\.\d+|sohu\.soooner\.com)[^"]+')
-all = preDes.findall(result)
 
 
 def polishAddress(all):
     block = 9
     return '\n'.join(all[block:2*block])
 
-with open('Now.txt', 'w') as f:
-    f.write(polishAddress(all))
+
+import re
+all = re.findall('http://(?:\d+\.\d+\.\d+\.\d+|sohu\.soooner\.com)[^"]+', result)
+
+if len(all) > 0:
+    name = input("File Name : ")
+    with open('Now.txt', 'w') as f:
+        f.write(name + '\n')
+        f.write(polishAddress(all))
+else:
+    print('No addresses found!!!!!!!!!!!!!!')
+
+r.destroy()
