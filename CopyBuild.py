a319d1c84b5cf47f533ddcd24e4a4ecbf8fe6b5b
folger <folger@originlab.com.cn>
2014-08-13 14:13:47 +0800

    improve CopyBuild.py

--- CopyBuild.py
+++ CopyBuild.py
@@ -1,24 +1,34 @@
 import os
-import shutil
-import zipfile
+from shutil import copytree
+from zipfile import ZipFile
+from functools import partial
 
 srcpath = r'\\fs1\NewBuilds\92\I'
 #srcpath = os.path.expanduser(r'~\Desktop\I')
 despath = r'\\fs1\Builds\92\I'
 
+pr = partial(print, end='', flush=True)
+newbuild = False
+
 try:
     for f in os.listdir(srcpath):
         if f not in os.listdir(despath):
-            print(f + ' ...')
+            newbuild = True
+            pr(f)
             srcfile = os.path.join(srcpath, f)
-            print("Copying ...")
-            shutil.copytree(srcfile, os.path.join(despath, f))
-            print("Zipping ...")
-            with zipfile.ZipFile(os.path.join(r'\\fs1\Released\ZipBuilds\92', f + '.zip'), 'w') as myzip:
+            pr('\tCopying ...')
+            copytree(srcfile, os.path.join(despath, f))
+            pr('\tZipping ...')
+            with ZipFile(os.path.join(r'\\fs1\Released\ZipBuilds\92', f + '.zip'),
+                                 'w') as myzip:
                 for rf in os.listdir(srcfile):
                     if rf.endswith('.db'):
                         continue
                     myzip.write(os.path.join(srcfile, rf), rf)
+            print()
 except Exception as e:
     print(e)
-    os.system('pause')
+
+if not newbuild:
+    print('No new build')
+os.system('pause')
