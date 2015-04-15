44a5c69d5eff564efd9aef2e70b435b5860c24f1
folger <folger@originlab.com.cn>
2015-04-15 11:07:11 +0800

    Origin py tools

--- Origin/empty_layer_name.py
+++ Origin/empty_layer_name.py
@@ -1,17 +1,17 @@
 import os
 import re
 
-path = 'H:/Dev/Origin'
+path = 'H:/Dev/Origin/Localization/J'
 pattern = re.compile(b'Pd\d' + 32 * b'\0')
 count = 0
 for f in os.listdir(path):
     if f.lower().endswith('.otp'):
         with open(os.path.join(path, f), 'rb') as fr:
             data = fr.read()
-        data1 = pattern.sub(b'Pd' + 33 * b'\0', data)
-        if data != data1:
+        al = pattern.findall(data)
+        if len(al) > 0:
             with open(os.path.join(path, f), 'wb') as fw:
-                print(f)
+                print(f, len(al), sep=' ')
                 count += 1
-                fw.write(data1)
+                fw.write(pattern.sub(b'Pd' + 33 * b'\0', data))
 print(count)
