23471baae08545358581415276665c242dcc1daa
folger <folger@originlab.com.cn>
2015-06-08 13:04:49 +0800

    polish sysvar.py

--- Origin/sysvar.py
+++ Origin/sysvar.py
@@ -2,45 +2,47 @@ import os
 import re
 
 
-codes = {'1':'FUNC_SYS_VALUE_DEF({_name})\n{{\n\treturn 1;\n}}',
-        '2':'FUNC_SYS_VALUE_FUNC_DEF({_name}, _func)',
-        '3': 'FUNC_SYS_VALUE_READONLY({_name}, _func)',
-        '4': 'FUNC_SYS_VALUE_GENERAL({_name}, _funcGet, _funcSet)',
-        '5': 'FUNC_SYS_VALUE_VAR_DEF({_name}, TYPE, _defaultVal)',
-        '6': 'static bool FUNC_VALID_CHECK({_name})(double& value)\n{{\n\treturn true;\n}}\nFUNC_SYS_VALUE_VAR_DEF_VALID_CHECK({_name}, TYPE, _defaultVal)',
-        '7': 'static double FUNC_GET_CONVERT({_name})(int value)\n{{\n\treturn 0;\n}}\nstatic int FUNC_SET_CONVERT({_name})(double value)\n{{\n\treturn 0;\n}}\nFUNC_SYS_VALUE_VAR_DEF_CONVERT({_name}, TYPE, _defaultVal)',
-        '8': 'FUNC_SYS_VALUE_GSTATE_BIT({_name}, _field, _bit)',
-        '9': 'FUNC_SYS_VALUE_GSTATE_BIT_REVERSE({_name}, _field, _bit)',
-        'a': 'FUNC_SYS_VALUE_GSTATE_VAR({_name}, _field)',
-        'b': 'static bool FUNC_VALID_CHECK({_name})(double& value)\n{{\n\treturn true;\n}}\nFUNC_SYS_VALUE_GSTATE_VAR_VALID_CHECK({_name}, _field)',
-        'c': 'FUNC_SYS_VALUE_COMMON_DWORD_BIT({_name}, _bit, _bReverseBVal)',
+codes = {
+        'v':	'FUNC_SYS_VALUE_VAR_DEF({_name}, TYPE, _defaultVal)',
+        'vv':	'static bool FUNC_VALID_CHECK({_name})(double& value)\n{{\n\treturn true;\n}}\nFUNC_SYS_VALUE_VAR_DEF_VALID_CHECK({_name}, TYPE, _defaultVal)',
+        'gb':	'FUNC_SYS_VALUE_GSTATE_BIT({_name}, _field, _bit)',
+        'gbr':	'FUNC_SYS_VALUE_GSTATE_BIT_REVERSE({_name}, _field, _bit)',
+        'gv':	'FUNC_SYS_VALUE_GSTATE_VAR({_name}, _field)',
+        'gvv':	'static bool FUNC_VALID_CHECK({_name})(double& value)\n{{\n\treturn true;\n}}\nFUNC_SYS_VALUE_GSTATE_VAR_VALID_CHECK({_name}, _field)',
+        'f':	'FUNC_SYS_VALUE_FUNC_DEF({_name}, _func)',
+        'fr':	'FUNC_SYS_VALUE_READONLY({_name}, [](){return;})',
+        'fg':	'FUNC_SYS_VALUE_GENERAL({_name}, [](){return;}, [](){})',
+        'vc':	'static double FUNC_GET_CONVERT({_name})(int value)\n{{\n\treturn 0;\n}}\nstatic int FUNC_SET_CONVERT({_name})(double value)\n{{\n\treturn 0;\n}}\nFUNC_SYS_VALUE_VAR_DEF_CONVERT({_name}, TYPE, _defaultVal)',
+        'cb':	'FUNC_SYS_VALUE_COMMON_DWORD_BIT({_name}, _bit, _bReverseBVal)',
+        'z':'FUNC_SYS_VALUE_DEF({_name})\n{{\n\treturn 1;\n}}',
         }
 
+okint2 = os.path.join(os.environ['Develop'], r'Source\vc32\okern96\okint2.cpp')
 def sys_value_format(name): return '\t\tSYS_VALUE_ENTRY({}),'.format(name)
 table_sign = 'static SYSVALUE l_values[] = {'
 
 while True:
     user = input('''
 Code Type:
-1. FUNC_SYS_VALUE_DEF(_name)
-2. FUNC_SYS_VALUE_FUNC_DEF(_name, _func)
-3. FUNC_SYS_VALUE_READONLY(_name, _func)
-4. FUNC_SYS_VALUE_GENERAL(_name, _funcGet, _funcSet)
-5. FUNC_SYS_VALUE_VAR_DEF(_name, TYPE, _defaultVal)
-6. FUNC_SYS_VALUE_VAR_DEF_VALID_CHECK(_name, TYPE, _defaultVal)
-7. FUNC_SYS_VALUE_VAR_DEF_CONVERT(_name, TYPE, _defaultVal)
-8. FUNC_SYS_VALUE_GSTATE_BIT(_name, _field, _bit)
-9. FUNC_SYS_VALUE_GSTATE_BIT_REVERSE(_name, _field, _bit)
-a. FUNC_SYS_VALUE_GSTATE_VAR(_name, _field)
-b. FUNC_SYS_VALUE_GSTATE_VAR_VALID_CHECK(_name, _field)
-c. FUNC_SYS_VALUE_COMMON_DWORD_BIT(_name, _bit, _bReverseBVal)
+v	FUNC_SYS_VALUE_VAR_DEF(_name, TYPE, _defaultVal)
+vv	FUNC_SYS_VALUE_VAR_DEF_VALID_CHECK(_name, TYPE, _defaultVal)
+gb	FUNC_SYS_VALUE_GSTATE_BIT(_name, _field, _bit)
+gbr	FUNC_SYS_VALUE_GSTATE_BIT_REVERSE(_name, _field, _bit)
+gv	FUNC_SYS_VALUE_GSTATE_VAR(_name, _field)
+gvv	FUNC_SYS_VALUE_GSTATE_VAR_VALID_CHECK(_name, _field)
+f	FUNC_SYS_VALUE_FUNC_DEF(_name, _func)
+fr	FUNC_SYS_VALUE_READONLY(_name, _func)
+fg	FUNC_SYS_VALUE_GENERAL(_name, _funcGet, _funcSet)
+vc	FUNC_SYS_VALUE_VAR_DEF_CONVERT(_name, TYPE, _defaultVal)
+cb	FUNC_SYS_VALUE_COMMON_DWORD_BIT(_name, _bit, _bReverseBVal)
+z	FUNC_SYS_VALUE_DEF(_name)
 System Variable Name & Code type: ''')
     if len(user) == 0:
         break
     name, codetype = user.split(' ')
     name = name.upper()
 
-    with open(os.path.join(os.environ['Develop'], 'Source/vc32/okern96/okint2.cpp')) as fr:
+    with open(okint2) as fr:
         data = fr.read()
 
     func_pos = data.find('template <class T, int N>')
@@ -62,5 +64,5 @@ System Variable Name & Code type: ''')
 
     data = data[:table_begin] + '\n'.join(sys_values) + data[table_end:]
 
-    with open(os.path.join(os.environ['Develop'], 'Source/vc32/okern96/okint2.cpp'), 'w') as fw:
+    with open(okint2, 'w') as fw:
         fw.write(data)
