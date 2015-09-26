19e95a9d90466bd289d6b908f7dc0ff03a9cdfb5
folger <folger6@gmail.com>
2015-09-26 17:15:42 +0800

    BuildCmd using ArgumentParser

--- BuildProj/BuildCmd.py
+++ BuildProj/BuildCmd.py
@@ -1,33 +1,52 @@
 import sys
 import os
+from argparse import ArgumentParser
 import BuildUtils
 
 
-platform = 'x64'
-configuration = 'Debug'
+parser = ArgumentParser()
+parser.add_argument('project')
+parser.add_argument('-f', '--file',
+                    help='Source file in specified project')
+parser.add_argument('-p', '--platform',
+                    help='Build Platform, can be Win32 or x64, Default Win32')
+parser.add_argument('-c', '--configuration',
+                    help='Build Configuration, can be Debug or Release, '
+                    'Debug by default')
+args = parser.parse_args()
+if args.platform is None:
+    args.platform = 'Win32'
+if args.configuration is None:
+    args.configuration = 'Debug'
 
 
 def main():
-    if len(sys.argv) > 1:
-        project_name = sys.argv[1]
+    for project, project_file in BuildUtils.get_projects():
+        if os.path.splitext(project)[0].lower() == args.project.lower():
+            print(os.path.dirname(project_file))
+            if args.file:
+                file_name = args.file
+                for ff in BuildUtils.get_project_files(project_file):
+                    if os.path.basename(ff).lower() == file_name.lower():
+                        print(BuildUtils.compile(True,
+                                                 project_file,
+                                                 args.platform,
+                                                 args.configuration,
+                                                 ff))
+                        break
+                else:
+                    raise ValueError('{} cannot be found in project {}'
+                                     .format(file_name, args.project))
+                return
 
-        for project, project_file in BuildUtils.get_projects():
-            if os.path.splitext(project)[0].lower() == project_name.lower():
-                print(os.path.dirname(project_file))
-                if (len(sys.argv) > 2):
-                    file_name = sys.argv[2]
-                    for file in BuildUtils.get_project_files(project_file):
-                        if os.path.basename(file).lower() == file_name.lower():
-                            print(BuildUtils.compile(True, project_file, platform, configuration, file))
-                            break
-                    else:
-                        raise ValueError('{} cannot be found in project {}'.format(file_name, project_name))
-                    return
-
-                print(BuildUtils.build(True, project_file, platform, configuration))
-                break
-        else:
-            raise ValueError('project file {} cannot be found'.format(project_name))
+            print(BuildUtils.build(True,
+                                   project_file,
+                                   args.platform,
+                                   args.configuration))
+            break
+    else:
+        raise ValueError('project file {} cannot be found'
+                         .format(args.project))
 
 
 try:
