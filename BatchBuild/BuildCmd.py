c921698d9c5d677c16b230db73da891dbdf9c103
folger <folger@originlab.com.cn>
2016-04-29 14:37:51 +0800

    Improve BuildCmd.py

--- BatchBuild/BuildCmd.py
+++ BatchBuild/BuildCmd.py
@@ -27,52 +27,58 @@ def guess_project_from_source_file(f):
     path = os.path.dirname(f)
     paths = path.split('/')
     last_folder = paths[-1].lower()
+    proj_name = ''
     if last_folder == 'sdk':
-        return 'oks70'
-    if last_folder == 'labutil':
-        return 'outl'
-    for ff in os.listdir(path):
-        if ff.endswith('.vcxproj'):
-            return os.path.splitext(ff)[0]
-    print('Failed to guess project from source file')
-    assert(False)
+        proj_name = r'vc32\okstatic\oks70'
+    elif last_folder == 'labutil':
+        proj_name = r'vc32\outl'
+    else:
+        for ff in os.listdir(path):
+            if ff.endswith('.vcxproj'):
+                return os.path.join(path, ff)
+    if proj_name:
+        return os.path.join(os.environ['Develop'],
+                            'Source',
+                            proj_name) + '.vcxproj'
+    raise ValueError('Failed to guess project from source file {}'.format(f))
+
+
+def find_project_file(proj):
+    for project, project_file in BuildUtils.get_projects():
+        if os.path.splitext(project)[0].lower() == proj.lower():
+            return project_file
+    raise ValueError('Failed to find project file from project name {}'
+                     .format(proj))
 
 
 def main():
     if args.project == 'xxx':
         if not args.file:
-            print('Source file is not specified, cannot guess project')
-            assert(False)
-        args.project = guess_project_from_source_file(args.file)
-    if args.file:
-        args.file = os.path.basename(args.file)
-    for project, project_file in BuildUtils.get_projects():
-        if os.path.splitext(project)[0].lower() == args.project.lower():
-            print(os.path.dirname(project_file))
-            if args.file:
-                file_name = args.file
-                for ff in BuildUtils.get_project_files(project_file):
-                    if os.path.basename(ff).lower() == file_name.lower():
-                        print(BuildUtils.compile(not args.all_output,
-                                                 project_file,
-                                                 args.platform,
-                                                 args.configuration,
-                                                 ff))
-                        break
-                else:
-                    raise ValueError('{} cannot be found in project {}'
-                                     .format(file_name, args.project))
-                return
-
-            print(BuildUtils.build(not args.all_output,
-                                   project_file,
-                                   args.platform,
-                                   args.configuration))
-            break
+            raise ValueError('Source file is not specified, '
+                             'cannot guess project')
+        project_file = guess_project_from_source_file(args.file)
     else:
-        raise ValueError('project file {} cannot be found'
-                         .format(args.project))
+        project_file = find_project_file(args.project)
+    print(os.path.dirname(project_file))
+    if args.file:
+        file_name  = os.path.basename(args.file)
+        for ff in BuildUtils.get_project_files(project_file):
+            if os.path.basename(ff).lower() == file_name.lower():
+                print(BuildUtils.compile(not args.all_output,
+                                         project_file,
+                                         args.platform,
+                                         args.configuration,
+                                         ff))
+                break
+        else:
+            raise ValueError('{} cannot be found in project {}'
+                             .format(file_name, args.project))
+        return
 
+    print(BuildUtils.build(not args.all_output,
+                           project_file,
+                           args.platform,
+                           args.configuration))
 
 try:
     main()
