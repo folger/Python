831ff484a0471594966e483e58aa7ab84b61d600
folger <folger@originlab.com.cn>
2017-03-13 14:30:42 +0800

    CopyDlls.py skip PyQt

--- BatchBuild/BatchBuildUtils.py
+++ BatchBuild/BatchBuildUtils.py
@@ -66,25 +66,7 @@ class DllJobThread(QThread):
 
 class CopyDllThread(DllJobThread):
     def beforeDoJobs(self, win32):
-        platformpath = '32bit' if win32 else '64bit'
-        self.path = os.path.join(r'\\fs1\Dev\{}_dlls'
-                                 .format(self.version()),
-                                 'win32' if win32 else 'x64')
-        if not os.path.isdir(self.path):
-            os.makedirs(self.path)
-        for the_file in os.listdir(self.path):
-            file_path = os.path.join(self.path, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except:
-                pass
-
-        os.makedirs(os.path.join(self.path, platformpath, 'PyDLLs'))
-        os.makedirs(os.path.join(self.path, platformpath, 'Py27DLLs'))
-        os.makedirs(os.path.join(self.path, r'OriginC\Originlab'))
+        self.path = before_copy_dlls(win32, self.version())
 
     def doJob(self, dll):
         shutil.copyfile(os.path.join(self.binfolder, dll),
@@ -141,6 +123,38 @@ def origin_version(dev_folder, default):
     current_branch = get_current_branch(dev_folder)
     m = re.match('(\d+)_release', current_branch)
     return m.group(1) if m else default
+
+
+def before_copy_dlls(win32, version):
+    platformpath = '32bit' if win32 else '64bit'
+    path = os.path.join(r'\\fs1\Dev\{}_dlls'
+                        .format(version),
+                        'win32' if win32 else 'x64')
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    for the_file in os.listdir(path):
+        file_path = os.path.join(path, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except:
+            pass
+
+    os.makedirs(os.path.join(path, platformpath, 'PyDLLs'))
+    os.makedirs(os.path.join(path, platformpath, 'Py27DLLs'))
+    os.makedirs(os.path.join(path, r'OriginC\Originlab'))
+    return path
+
+
+def copy_dlls(binfolder, win32, version, updated):
+    dlls = get_origin_binaries(binfolder, win32, version)
+    path = before_copy_dlls(win32, version)
+    for i, dll in enumerate(dlls):
+        updated(i, dll)
+        shutil.copyfile(os.path.join(binfolder, dll),
+                        os.path.join(path, dll))
 
 
 if __name__ == '__main__':
--- BatchBuild/CopyDlls.py
+++ BatchBuild/CopyDlls.py
@@ -2,14 +2,11 @@ import os
 import sys
 import json
 
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-
 import BatchBuildUtils
 
 
 def _updated(i, s):
-    print('[{}] {} {}'.format(_version, i + 1, s))
+    print('[{}] ({}) {}'.format(_version, i + 1, s))
 
 dev_folder = sys.argv[1]
 
@@ -18,16 +15,14 @@ with open('settings.json') as f:
     MASTER_VERSION = settings['MasterVersion']
 _version = BatchBuildUtils.origin_version(dev_folder, MASTER_VERSION)
 
-app = QApplication([])
-mt = BatchBuildUtils.CopyDllThread(None, _version, app)
-mt.binfolder = os.path.join(dev_folder, 'Origin')
-mt.updated.connect(_updated)
-mt.win32 = False
-mt.x64 = False
+platforms = []
 for c in sys.argv[2:]:
     if c == 'Win32':
-        mt.win32 = True
+        platforms.append(True)
     elif c == 'x64':
-        mt.x64 = True
-mt.start()
-app.exec()
+        platforms.append(False)
+for p in platforms:
+    BatchBuildUtils.copy_dlls(os.path.join(dev_folder, 'Origin'),
+                              p,
+                              _version,
+                              _updated)
